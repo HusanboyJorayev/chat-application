002543ee98bda863cd4fd8bea8555ecb930243e4
HusanboyJorayev <144492116+HusanboyJorayev@users.noreply.github.com>
2024-01-30 19:20:03 +0500

    update success 100%

--- src/main/java/com/example/javaprojectspring_boot/JavaProjectSpringBootApplication.java
+++ src/main/java/com/example/javaprojectspring_boot/JavaProjectSpringBootApplication.java
@@ -1,10 +1,17 @@
 package com.example.javaprojectspring_boot;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(
+		servers = @Server(
+				url = "http://213.230.64.93:8181"
+		)
+)
 @Tag(name = "Chat-Application Api")
 public class JavaProjectSpringBootApplication {
 
